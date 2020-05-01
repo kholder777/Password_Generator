bf2735f8fb3d744b11e4d1bb035f9cfa20eeeae1
Kevin Holder <kholder777@yahoo.com>
2020-05-01 17:14:42 -0500

    added some js pseudocode. This closes #18

---
+++ script.js
@@ -0,0 +1,11 @@
+// link checkbox from html
+
+// link length slider from html
+
+// link generate button from html
+
+// link clipboard button to html
+
+// add event listener for generate button
+
+// add event listeners for checkboxes
